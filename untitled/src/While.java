0c3d055b81cd62eac3f1260ce66c540b198b59e4
Lenaclochkova <lenaclochkova@gmail.com>
2018-10-07 00:07:02 +0300

    heh

---
+++ untitled/src/While.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner( System.in );
+        int N = in.nextInt();
+        int[] a = new int[N];
+        for (int i = N; i < a.length; i++) {
+            System.out.print("[");
+//        for (int i = N; i >0; i-=N) {
+//            System.out.print("[");
+//        }
+//        for (int i = N; i >0; i-=N) {
+//            System.out.print("]");
+//
+//        }
+//        for (int i = N; i >0; i--) {
+//
+//            System.out.print("[");
+//        }
+//
+//        for (int i = N; i >0; i--) {
+//            System.out.print("]");
+//        }
+
+        }
+    }
+}
